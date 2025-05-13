392baa3bc02d0ab1119b9e314ebb5f0332a2e60a
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2025-05-13 16:09:08 +0530

    Six commit

---
+++ alphabet_or_not.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character : ");
+    scanf("%c",&ch);
+
+    if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z')) {
+        printf("It is an alphabet");
+    } else {
+        printf("Not an alphabet");
+    }
+    return 0;
+}
