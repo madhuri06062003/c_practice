e377ae633475a2bb2931e047f79a43896c477319
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2025-05-18 15:51:54 +0530

    34

---
+++ sub_matrix_if.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main() {
+    int i,j,row,col;
+    int matrix1[10][10],matrix2[10][10],sub[10][10];
+
+    printf("Enter row : ");
+    scanf("%d",&row);
+    printf("Enter col : ");
+    scanf("%d",&col);
+
+    printf("Enter elements : ");
+    for(i=0;i<row;i++) {
+        for(j=0;j<col;j++) {
+            scanf("%d",&matrix1[i][j]);
+        }
+    }
+    for(i=0;i<row;i++) {
+        for(j=0;j<col;j++) {
+            scanf("%d",&matrix2[i][j]);
+        }
+    }
+    for(i=0;i<row;i++) {
+        for(j=0;j<col;j++) {
+            sub[i][j]=matrix1[i][j]+matrix2[i][j];
+        }
+        printf("Sub of matrix\n");
+        for(i=0;i<row;i++) {
+            for(j=0;j<col;j++) {
+                printf("%d",sub[i][j]);
+            }
+        }
+        printf("\n");
+        return 0;
+    }
+}
