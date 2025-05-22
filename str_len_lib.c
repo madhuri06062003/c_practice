aca82205ebe3473aa01ce62b2e6b91799ea42a25
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2025-05-22 16:43:26 +0530

    49

---
+++ str_len_lib.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    char str[100];
+    int len=0;
+
+    printf("Enter string : ");
+    fgets(str,sizeof(str),stdin);
+
+    while(str[len]!='\0' && str[len]!='\n') {
+        len++;
+    }
+    printf("Str length %d\n",len);
+    return 0;
+}
