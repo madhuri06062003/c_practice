9907ccf18d3c51777a858afc020e769f0ed02bfd
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2025-05-25 14:27:20 +0530

    49

---
+++ str.dec.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+    int i,j;
+    char temp;
+    char str[10];
+
+    printf("Enter str : ");
+    fgets(str,sizeof(str),stdin);
+
+    str[strcspn(str,"\n")]='\0';
+
+    int len =strlen(str);
+    for(i=0;i<len-1;i++) {
+        for(j=i+1;j<len;j++) {
+            if(str[i]<str[j]) {
+                temp=str[i];
+                str[i]=str[j];
+                str[j]=temp;
+            }
+        }
+    }
+    printf("Descending %s\n",str);
+    return 0;
+}
