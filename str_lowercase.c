7fc4f63351d54c9fa5def7f038ae63012561b3b9
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2025-05-26 16:49:06 +0530

    56

---
+++ str_lowercase.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int main() {
+    char str[100];
+    int i;
+
+    printf("Enter str :");
+    fgets(str,sizeof(str),stdin);
+
+    str[strcspn(str,"\n")]=0;
+
+    for(i=0;str[i]!='\0';i++) {
+        str[i]=tolowe(str[i]);
+    }
+    printf("Lowecase %s\n",str);
+    return 0;
+}
