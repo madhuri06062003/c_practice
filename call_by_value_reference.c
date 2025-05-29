17faf2c7097674eb84289c39221cc517092cf4e7
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2025-05-29 20:57:26 +0530

    a

---
+++ call_by_value_reference.c
@@ -0,0 +1,4 @@
+call by value : send a copy of the variable to the function
+                the original value does not change
+call by reference : send an address of the variable to the function
+                     the original value can be changed
