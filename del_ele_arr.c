c29fbb8ea084ea5b19208ffeaf43447c09e8988d
madhuri06062003 <119513611+madhuri06062003@users.noreply.github.com>
2025-05-30 15:47:06 +0530

    Update del_ele_arr.c

--- del_ele_arr.c
+++ del_ele_arr.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-void deletearr(int arr[],int *n,int position) {
+void deletearr(int arr[],int n,int position) {
     if(position <0 || position>=*n) {
         printf("Invalid position");
         return;
 
     }   
-    for(int i=position;i<*n-1;i++) {
+    for(int i=position;i<n-1;i++) {
         arr[i]=arr[i+1];
     } 
-    (*n)--;
+    (n)--;
 }
 int main() {
     int arr[10],n,i,position;
