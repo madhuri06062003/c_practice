4169e3d599d142dde15f7bd9dc8881323d0a79d4
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2025-06-17 12:38:20 +0530

    viven

---
+++ add_even_or_odd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+    int num1,num2,sum;
+    int i=0;
+    printf("Enter num1 :");
+    scanf("%d",&num1);
+    printf("Enter num2 :\n");
+    scanf("%d",&num2);
+
+    sum = num1+num2;
+    printf("Enter sum of numbers%d\n",sum);
+
+    if(i%2==0) {
+        printf("Even number");
+    }else{
+        printf("odd number");
+    }
+    return 0;
+}
