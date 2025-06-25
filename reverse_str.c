d84475c1b3a743e65021ec63c2b890f2af73d2d6
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2025-06-25 11:28:26 +0530

    a

--- reverse_str.c
+++ reverse_str.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
+/*#include<stdio.h> //arr
 int main() {
     int arr[]={1,2,3,4,5,6};
     int i;
-    int length =0;
+    int length =sizeof(arr)/sizeof(arr[0]);
     
     for(i=0;i<length/2;i++) {
         int temp=arr[i];
@@ -13,4 +13,23 @@ int main() {
         printf("arr[%d] = %d\n",i,arr[i]);
     }
     return 0;
+}*/
+
+//str
+#include<stdio.h>
+#include<string.h>
+int main() {
+    char str[]="Hello";
+    int i;
+    int length;
+
+    length = strlen(str);
+
+    for(i=0;i<length/2;i++) {
+        char temp=str[i];
+        str[i]=str[length-i-1];
+        str[length-i-1]=temp;
+    }
+        printf("Reversed str %s\n",str);
+    
 }
